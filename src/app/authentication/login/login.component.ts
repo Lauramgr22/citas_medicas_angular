ba3f8f089f78d581fa727dd7542c0a9793f5059c
Lauramgr22 <lauramgr6@gmail.com>
2024-05-15 14:47:47 -0500

    Feat: Se configura inicio de sesion

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth/auth.service';
 import { routes } from 'src/app/shared/routes/routes';
 
@@ -13,18 +14,18 @@ export class LoginComponent implements OnInit {
   public passwordClass = false;
 
   form = new FormGroup({
-    email: new FormControl('admin@dreamguys.in', [
+    email: new FormControl('testing@test.com', [
       Validators.required,
       Validators.email,
     ]),
-    password: new FormControl('123456', [Validators.required]),
+    password: new FormControl('12345678', [Validators.required]),
   });
 
   get f() {
     return this.form.controls;
   }
 
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, public router: Router) { }
   ngOnInit(): void {
     if (localStorage.getItem('authenticated')) {
       localStorage.removeItem('authenticated');
@@ -33,9 +34,25 @@ export class LoginComponent implements OnInit {
 
   loginFormSubmit() {
     if (this.form.valid) {
-      this.auth.login();
+      this.auth.login(this.form.value.email ? this.form.value.email : '', this.form.value.password ? this.form.value.password : '')
+        .subscribe({
+          next: (resp: any) => {
+            if (resp){
+              //Login exitoso
+              this.router.navigate([routes.adminDashboard]);
+            } else{
+              //Login no exitoso
+              alert("El usuario o cotrasena son incorrectos o no existen");
+            }
+            console.log(resp);
+          },
+          error: (error) => {
+            console.log(error);
+          }
+        });
     }
   }
+  
   togglePassword() {
     this.passwordClass = !this.passwordClass;
   }
--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { catchError, map, of } from 'rxjs';
+import { URL_SERVICIOS } from 'src/app/config/config';
 // import { BehaviorSubject } from 'rxjs';
 import { routes } from '../routes/routes';
 
@@ -8,11 +11,44 @@ import { routes } from '../routes/routes';
 })
 export class AuthService {
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    public http: HttpClient,
+    ) {}
 
-  public login(): void {
-    localStorage.setItem('authenticated', 'true');
-    this.router.navigate([routes.adminDashboard]);
-    //let URL= URL_SERVICIOS+"/auth/login";
+  login(email:any, password:any){
+    //localStorage.setItem('authenticated', 'true');
+    //this.router.navigate([routes.adminDashboard]);
+    let URL= URL_SERVICIOS +"/auth/login";
+    return this.http.post(URL,{email:email, password:password}).pipe(
+      map((auth:any) => {
+        console.log(auth);
+        const result= this.saveLocalStorage(auth);
+        return result;
+      }),
+      catchError((error:any) =>{
+        console.log(error);
+        return of(undefined)
+      })
+    );
   }
+
+  saveLocalStorage(auth:any){
+    if(auth && auth.acces_token){
+      localStorage.setItem("token", auth.acces_token);
+      localStorage.setItem("user", JSON.stringify(auth.user));
+      localStorage.setItem('authenticated', 'true'); //Aqui se realiza la redireccion
+      return true;
+    }
+    return false;
+  }
+
+  logout(){
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    localStorage.removeItem("authenticated");
+    this.router.navigate([routes.login]);
+  }
+
 }
+
